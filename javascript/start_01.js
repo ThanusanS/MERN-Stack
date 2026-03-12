823ce70df8de4f221f9d9875696823d0d25f5b41
ThanusanS <thanusan0411@gmail.com>
2026-03-12 20:41:47 +0530

    Added File

--- javascript/start_01.js
+++ javascript/start_01.js
@@ -1,1 +1,9 @@
 console.log("Hello")
+
+const name = "John"
+const age = 30
+console.log("My name is " + name + " and I am " + age + " years old.")
+
+// Using template literals
+console.log(`My name is ${name} and I am ${age} years old.`)
+
